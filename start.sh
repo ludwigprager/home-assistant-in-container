aa86fc906a1d7242adc8b9583a5ede13c2f0b6db
root <root@pi9.g1>
2023-02-17 10:51:44 +0100

    uses spedific tag

--- start.sh
+++ start.sh
@@ -1,5 +1,7 @@
 #/bin/bash
 
+TAG=2023.3.0.dev20230216
+
 docker run -d \
   --name ha \
   --privileged \
@@ -7,6 +9,6 @@ docker run -d \
   -v $(pwd)/config:/config \
   --network=host \
   --device /dev/ttyUSB0:/dev/ttyUSB0 \
-  ghcr.io/home-assistant/home-assistant:latest
+  ghcr.io/home-assistant/home-assistant:${TAG}
 
 #e TZ=MY_TIME_ZONE \
