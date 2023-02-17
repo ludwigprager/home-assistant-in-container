05abc0fd40fc7fa935678ded24d448215f1a67a0
root <root@pi9.g1>
2023-02-17 09:00:16 +0100

    restart script

--- start.sh
+++ start.sh
@@ -7,6 +7,6 @@ docker run -d \
   -v $(pwd)/config:/config \
   --network=host \
   --device /dev/ttyUSB0:/dev/ttyUSB0 \
-  ghcr.io/home-assistant/home-assistant:stable
+  ghcr.io/home-assistant/home-assistant:latest
 
 #e TZ=MY_TIME_ZONE \
